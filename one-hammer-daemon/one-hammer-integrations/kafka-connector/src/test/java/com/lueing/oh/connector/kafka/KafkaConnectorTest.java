fc142b9130e15fbe96488df1e02a21581bfeb7ca
canbin.zhang <yesido0725@gmail.com>
2023-04-30 13:00:07 +0800

    kafka 各种限制设置最大消息是8MB, 我们应用限制最大消息是4MB

--- one-hammer-daemon/one-hammer-integrations/kafka-connector/src/test/java/com/lueing/oh/connector/kafka/KafkaConnectorTest.java
+++ one-hammer-daemon/one-hammer-integrations/kafka-connector/src/test/java/com/lueing/oh/connector/kafka/KafkaConnectorTest.java
@@ -16,26 +16,26 @@ public class KafkaConnectorTest {
     public void testProducer() {
         Producer<String, String> producer = KafkaConnector.createProducer("",
                 Connector.builder().enableKerberos(false).bootstrapServers("192.168.0.16:9092").build(), "tx-01");
-//        StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
 
-//        for (int i = 0; i < 8 * 1024 * 1024; i++) {
-//            builder.append('a');
-//        }
+        for (int i = 0; i < 4 * 1024 * 1024; i++) {
+            builder.append('a');
+        }
 
         producer.beginTransaction();
-        for (int i = 0; i < 64; i++) {
-            producer.send(new ProducerRecord<>("topic-1", "k-" + i, "v-1-1"));
-        }
-//        producer.send(new ProducerRecord<>("topic-1", "k-1", "v-1-1"));
-//        producer.send(new ProducerRecord<>("topic-1", "k-2", "v-2-2"));
-//        producer.send(new ProducerRecord<>("topic-1", "k-3", builder.toString()));
+//        for (int i = 0; i < 64; i++) {
+//            producer.send(new ProducerRecord<>("topic-1", "k-" + i, "v-1-1"));
+//        }
+        producer.send(new ProducerRecord<>("topic-1", "k-1", "v-1-1"));
+        producer.send(new ProducerRecord<>("topic-1", "k-2", "v-2-2"));
+        producer.send(new ProducerRecord<>("topic-1", "k-3", builder.toString()));
         producer.commitTransaction();
     }
 
     @Test
     public void testConsumer() {
         Consumer<String, String> consumer = KafkaConnector.createConsumer("", Connector.builder().enableKerberos(false)
-                .bootstrapServers("192.168.0.16:9092").txId("tx-01").build(), "topic-1", "group-1");
+                .bootstrapServers("192.168.0.16:9092").build(), "topic-1", "group-1");
 
         for (int i = 0; i < 8; i++) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(10));
